b9b15d7313d55e3462752faa1a129a10ce238381
Пользователь <olagvas94@gmail.com>
2021-01-16 20:06:01 +0200

    initialization cities works

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,6 +5,9 @@ class View {
         }
     makeCityBlock(response, imageurl, id) {
         const div = document.createElement('div');
+        const deleteButton = document.createElement('button');
+        deleteButton.id = 'delete';
+        deleteButton.innerHTML = "DELETE CITY";
         div.classList.add('city');
         div.setAttribute('id', id);
         this.container.append(div);
@@ -13,7 +16,7 @@ class View {
         img.setAttribute('src', imageurl)
         const span = document.createElement('span');
         p.innerHTML = response.name;
-        div.append(p, img);
+        div.append(p, img, deleteButton);
     }    
 }
 class Model {
@@ -38,6 +41,27 @@ class Model {
                 this.addCityDb(response);
                 
             })
+        .catch(err => console.log(err))
+    
+    }
+    initCityWeather(cityname, id) {
+        let apiKey = "14e4636d12aa76d46bf7bef58bb56add";
+        let url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&appid=" + apiKey;
+        let promise = fetch(url);
+        promise
+        .then(response => {
+            if (response.ok && response.status === 200) {
+                
+                return response.json();
+            } else {
+                return Promise.reject(response.status);
+            }
+        })
+        .then(response => {
+            let imageurl = `http://openweathermap.org/img/wn/${response['weather'][0].icon}@2x.png`;
+            this.view.makeCityBlock(response, imageurl, id);
+                
+        })
         .catch(err => console.log(err))
     
     }
@@ -66,6 +90,22 @@ class Model {
         })
         .catch(error => console.log(error))
     }
+    initCities () {
+    let promise = fetch('http://localhost:3333/cities/');
+    promise
+    .then(response => {
+        if (response.ok && response.status === 200) {
+                    return response.json();
+                                  
+                } else {
+                    Promise.reject(response.status)
+                }
+    })
+    .then(response => response.forEach(item => {
+             this.initCityWeather(item.name, item._id);
+         }))
+    .catch(err => console.log(err))
+    }
 }
 class Controller {
     constructor (model) {
@@ -84,6 +124,7 @@ let view = new View();
 let model = new Model(view);
 let controller = new Controller(model);
 controller.watchClicks();
+controller.model.initCities();
 
 // document.getElementById('container').addEventListener('click', (event) => {
 //     console.log(event.target.id);
